15b536dd324377e030af34966028d335d6002129
BaczekAdam <35337720+BaczekAdam@users.noreply.github.com>
2021-02-25 15:46:42 +0100

    Repair AddCargo method in Warehouse

--- src/MainPackage/Cargo.java
+++ src/MainPackage/Cargo.java
@@ -1,28 +1,37 @@
 package MainPackage;
 
 import java.util.HashSet;
+import java.time.LocalDate;
 
 public class Cargo {
 
+    private String cCategory;
     private String cDescription;
+
+    LocalDate cArrivalDate;
     HashSet<Package> cPackages = new HashSet<>();
 
     public float cMass;
 
-    Cargo(String description) {
+    Cargo(String description, LocalDate arrivalDate) {
         this.cDescription = description;
+        cCategory = "Industrial";
+        cArrivalDate = arrivalDate;
     }
 
-    public void addPackage(Package pack){
+    public void addPackage(Package pack) {
         cPackages.add(pack);
         calculateMass();
     }
 
-    private void calculateMass()
-    {
+    private void calculateMass() {
         cMass = 0.0f;
-        for(Package pack : cPackages)
+        for (Package pack : cPackages)
             cMass += pack.pMass;
+    }
+
+    public void displayDetails() {
+
     }
 
     @Override
--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -1,13 +1,19 @@
 package MainPackage;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        ArrayList<Warehouse> warehouses = new ArrayList<>();
         //początek programu wstępny tekst i testowe zmienne
         System.out.println("-----WAREHOUSES-----");
 
+        LocalDate date1 = LocalDate.now();
 
         String loc1 = "Łódz, Piotrkowska";
         String loc2 = "Warszawa, Obywatelska";
@@ -20,28 +26,34 @@ public class Main {
 
         Warehouse war1 = new Warehouse(loc1, 20.0f);
         Warehouse war2 = new Warehouse(loc2, 5.0f);
-        Cargo cargo1 = new Cargo(descr1);
-        Cargo cargo2 = new Cargo(descr2);
+        Cargo cargo1 = new Cargo(descr1, date1);
+        Cargo cargo2 = new Cargo(descr2, date1);
         Package pack1 = new Package(pDesc1, 1.0f);
         Package pack2 = new Package(pDesc2, 2.0f);
         Package pack3 = new Package(pDesc3, 3.0f);
         Package pack4 = new Package(pDesc4, 4.0f);
 
-        cargo1.addPackage(pack1);
-        cargo1.addPackage(pack2);
-        cargo2.addPackage(pack3);
-        cargo2.addPackage(pack4);
+        warehouses.add(war1);
+        warehouses.add(war2);
 
-        war1.AddCargo(cargo1);
-        war1.AddCargo(cargo2);
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        int choice;
 
+        while (!exit) {
+            for (Warehouse war : warehouses)
+                System.out.println(war.toString());
 
-        System.out.println(war1.toString());
+            System.out.println("Options:");
+            choice = scanner.nextInt();
 
-        war1.moveCargoToAnotherWarehouse(cargo2,war2);
+            switch (choice) {
+                case 1 -> System.out.println("Wybrano 1");
+                case 5 -> exit = true;
+                default -> System.out.println("Wybrano niepoprawną opcję");
+            }
 
-        System.out.println(war1.toString());
-        System.out.println(war2.toString());
+        }
 
     }
 }
